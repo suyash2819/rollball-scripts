460578c07b869c48525e3543a8fe0d9130c7f0eb
suyash mandhana <mandhanasuyash@yahoo.in>
2017-07-27 00:11:53 +0530

    Add files via upload

---
+++ cameracontroller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour {
+	public GameObject player;//reference to the player
+	// Use this for initialization
+	private Vector3 offset;
+	void Start () {
+		offset = transform.position - player.transform.position;//difference between player and camera position
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {//for camera position and all it is best to use late update.
+		transform.position = player.transform.position + offset;//camera will move to a position as we move the ball. 
+	}
+}
---
+++ playercontroller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+
+	public float speed;//public so that it is shown in inspector to chnge there itself otherwise we need to open the script again and change.
+	public Text countText;
+	private Rigidbody rb;//for creating  reference to the rigidbody;
+	private int count;//for counting the cubes we have collected;
+	public Text WinText;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		WinText.text = "";
+	}
+	void FixedUpdate()
+	{
+		float movehorizontal = Input.GetAxis ("Horizontal");
+		float movevertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (movehorizontal,0.0f, movevertical);/*determines the direction of force on body ...x is horizontal,z
+																	is vertical and y=0 as we do not want to move up*/
+		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		//Destroy(other.gameObject); here we do not destroy instead we deactivate the other object the game object will touch.
+		if (other.gameObject.CompareTag ("pick up"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+
+	}
+	void SetCountText()
+	{
+		countText.text = "Count : " + count.ToString ();
+		if (count >= 11)
+		{
+			WinText.text="You Win";
+		}
+	}
+}
